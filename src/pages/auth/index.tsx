db99705183709be74de4c6898018b4ca65a478d3
Maria Paz Abarca <mp.abarca.alvear@gmail.com>
2023-05-28 01:07:07 +0200

    Add loading inspector on auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,13 +2,27 @@
 import Navbar from '@/components/molecules/Navbar';
 import AuthModal from '@/components/organisms/AuthModal';
 import { authModalState } from '@/recoil/authModalAtom';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
 
 type AuthPageProps = {};
 
 const AuthPage:React.FC<AuthPageProps> = () => {
+    const [pageLoading, setPageLoading] = useState<boolean>(true);
     const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+    const router = useRouter();
+
+    useEffect(() => {
+        if(user) router.push('/');
+        if(!user && !loading) setPageLoading(false);
+    }, [user, router, loading]);
+
+    if(pageLoading) return null;
+
     return(
         <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
             <div className='max-w-7xl mx-auto'>
